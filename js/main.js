d9ff4474a7eab079c1e7163527ce187d01a3a853
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-31 16:45:33 +0100

    first-part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-	//Code
+  $.ajax(
+  {
+    'url' : 'https://flynn.boolean.careers/exercises/api/array/music',
+    'method' : 'GET',
+    'success': function (data) {
+      console.log(data.response);
+      processData(data.response);
+
+    },
+    'error' : function (request, state, errors) {
+      alert('Errore ' + errors);
+    }
+  }
+);
+
 });
+
+
+function processData(cds) {
+  for (var i = 0; i < cds.length; i++) {
+    var cd = cds[i];
+    console.log(cd);
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+
+    var html = template(cd);
+    $('.cds-container').append(html);
+  }
+}
